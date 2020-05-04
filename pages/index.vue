80b3d1d0a1da40e90504f5529b2ceb43347750e0
Eugene Nakoneschniy <evgeniy.nakoneschniy@suttermills.com>
2020-05-04 19:47:17 +0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script>
+
+import TodoList from '../components/TodoList'
+export default {
+  components: { TodoList }
+}
+</script>
